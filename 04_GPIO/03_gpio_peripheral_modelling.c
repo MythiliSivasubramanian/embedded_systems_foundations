af4b9213fa0b6dbcfca2b212ada91caf7877a81e
Mythili Sivasubramanian <sivasubramanianmythili@gmail.com>
2026-07-29 15:27:59 +0200

    Model of complete GPIO peripherals and access whole register (Read, write and toggle) and access single p in (Set a pin, clear a pin, Read a pin and toggle a pin )

--- 04_GPIO/03_gpio_peripheral_modelling.c
+++ 04_GPIO/03_gpio_peripheral_modelling.c
@@ -1,83 +1,124 @@
 /*
   03_gpio_peripheral_modelling.c
   Modelling GPIO pheripheral using C Structures
- 
-  - A peripheral is a collection of registers.
-  - The structure represents the register layout and the pointer connects the structure 
-    to the actual hardware address.
- */
+*/
 
 #include <stdint.h>
+
 /*
-  GPIO peripheral register layout
-  Each member represents one hardware register.
-  The compiler automatically creates offsets:
+  GPIO peripheral register layout Offset as per STM32F407VG reference manaual
   MODER   -> 0x00
   OTYPER  -> 0x04
   OSPEEDR -> 0x08
   PUPDR   -> 0x0C
   IDR     -> 0x10
   ODR     -> 0x14 
+  BSRR    -> 0x18
+  LCKR    -> 0x1C
+  AFRL    -> 0x20
+  AFRH    -> 0x24
  */
 
+// The structure represents the Hadware register layout
 typedef struct
 {
-    volatile uint32_t MODER;      // Offset 0x00
-    volatile uint32_t OTYPER;     // Offset 0x04
-    volatile uint32_t OSPEEDR;    // Offset 0x08
-    volatile uint32_t PUPDR;      // Offset 0x0C
-    volatile uint32_t IDR;        // Offset 0x10
-    volatile uint32_t ODR;        // Offset 0x14
-
+    volatile uint32_t MODER;      
+    volatile uint32_t OTYPER;     
+    volatile uint32_t OSPEEDR;    
+    volatile uint32_t PUPDR;      
+    volatile uint32_t IDR;        
+    volatile uint32_t ODR;        
+    volatile uint32_t BSRR;       
+    volatile uint32_t LCKR;       
+    volatile uint32_t AFRL;       
+    volatile uint32_t AFRH;       
+    
 } GPIO_TypeDef;
 
 /*
   GPIO peripheral base addresses
   STM32F407VG Reference Manual:
-  GPIOA base = 0x40020000
-  GPIOB base = 0x40020400 
- */
+*/
 
-#define GPIOA_BASE   0x40020000UL
-#define GPIOB_BASE   0x40020400UL
+#define GPIOA ((volatile GPIO_TypeDef *) 0x40020000U)
+#define GPIOB ((volatile GPIO_TypeDef *) 0x40020400U)
+#define GPIOC ((volatile GPIO_TypeDef *) 0x40020800U)
+#define GPIOD ((volatile GPIO_TypeDef *) 0x40020C00U)
+#define GPIOE ((volatile GPIO_TypeDef *) 0x40021000U)
+#define GPIOF ((volatile GPIO_TypeDef *) 0x40021400U)
+#define GPIOG ((volatile GPIO_TypeDef *) 0x40021800U)
+#define GPIOH ((volatile GPIO_TypeDef *) 0x40021C00U)
+#define GPIOI ((volatile GPIO_TypeDef *) 0x40022000U)
 
-/*
-  Creating peripheral instances
-  Same structure.
-  Different base address.
- 
-  GPIOA pointer points to GPIOA hardware registers.
-  GPIOB pointer points to GPIOB hardware registers.
- */
-
-GPIO_TypeDef GPIOA = (GPIO_TypeDef )GPIOA_BASE;
-GPIO_TypeDef GPIOB = (GPIO_TypeDef )GPIOB_BASE;
 
 int main(void)
 {
 
-    /*
-      Accessing registers using structure members.
-      GPIOA->ODR means: GPIOA base address + ODR offset
-      0x40020000 + 0x14 = 0x40020014
-    */
-
-    GPIOA->ODR = 0x01;
-
-    // GPIOA->IDR means: 0x40020000 + 0x10 = 0x40020010
+  /*
+  1. Whole Resgiter operations:
+    a. Write a whole register
+    b. Read a whole register
+    c. Toggle a whole register (dangerous)
+  
+  2. Individual Pin Operations:
+    a. Write (Set) a single pin (Write 1)
+    b. Clear a single pin (Write 0)
+    c. Read a single pin
+    d. Toggle a single pin
     
-    uint32_t pin_state;
-    pin_state = GPIOA->IDR;
-
-    /*
-      GPIOB uses the same structure. Only the base address changes.
-     GPIOB->ODR is 0x40020400 + 0x14 = 0x40020414
-    */
-
-    GPIOB->ODR = 0x01;
-    while(1)
+  */
+ 
+  //  1. Whole Resgiter operations:
+  /* 1.a. Write whole register*/
+  
+  // Write 0x05 to GPIOB_ODR (Write 0x05 at 0x40020414 (GPIOB address. + ODR Offset))
+  GPIOB -> ODR = 0x05;       // Same as (*GPIOB).ODR = 0x05
+  
+  /* 1.b. Read whole regsiter*/
+  // A variable to store the register state
+  uint32_t register_state;
+  register_state = GPIOB -> IDR;
+  
+  
+  /* 1.c. Toggle whole register (XOR) */
+  /* Not recommended as it flips all bits, whcih may
+  accidently change connections to other hardware
+  */
+ 
+  GPIOB->ODR ^= 0xFFFFFFFFU;  // Same as (*GPIOB).ODR ^= 0xFFFFFFFFU;
+  // Same as (*GPIOB).ODR = (*GPIOB).ODR ^ 0xFFFFFFFFU;
+  // Same as GPIOB->ODR = GPIOB->ODR ^ 0xFFFFFFFFU;
+  
+  
+  //2. Individual Pin Operations:
+  
+  /* 2.a. Write / Set a single pin (write 1) */
+  // Write GPIOB Pin5 1
+  GPIOB->ODR |= (1U << 5);
+  
+  /* 2.b. Clear a single pin (write 0)*/
+  // Clear a single pin GPIOB_ODR PIN 5
+  GPIOB->ODR &=  ~(1U << 5);
+ 
+  /* 2.c. Read a single pin */
+  // Read a single pin GPIOC_IDR PIN 5
+  if(GPIOC->IDR & (1U << 5))
+    // or if ((GPIOC->IDR & (1U << 5)) != 0)
     {
-
+      // pin5 is 1
+    } 
+  else
+    {
+      // pin5 is 0
     }
-
+  
+  
+  /* 2.d. Toggle a single pin */
+  // Toggle a single pin GPIOB_ODR PIN 2
+  GPIOB->ODR ^= (1U << 2);
+  
+  
+  
+  return 0;
 }
+
